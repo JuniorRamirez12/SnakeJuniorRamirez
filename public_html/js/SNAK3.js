bf537bb736af47d01dba7134f97918b46be65479
Junior Ramirez <Juniorr87@yahoo.com>
2014-10-13 14:07:06 -0700

    update on snake 10/13/14

--- public_html/js/SNAK3.js
+++ public_html/js/SNAK3.js
@@ -1,13 +1,33 @@
+/*----------------------------------------------------------------------------
+ * Variables
+ * ---------------------------------------------------------------------------
+ */
+
 var snake;
 var snakeLength;
 var snakeSize;
+var snakeDirection;
+
+var food;
 
 var context;
 var screenWidth;
 var screenHeight;
 
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ * Executing Game Code
+ * ##########################################################################
+ */
+
 gameInitialize();
-gameDraw();
+snakeInitialize();
+foodInitialize();
+setInterval(gameLoop, 1000/30);
+
+/*---------------------------------------------------------------------------
+ * Game Functions
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+ */
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -21,7 +41,10 @@ function gameInitialize() {
 }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
 }
 
 function gameDraw() {
@@ -29,18 +52,68 @@ function gameDraw() {
     context.fillRect(0, 0, screenWidth, screenHeight);
 } 
 
+/*----------------------------------------------------------------------------
+ * Snake Functions
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ */
+
 function snakeInitialize() {
     snake = [];
-    snakeLength = 15;
-    snakeSize = 20;
+    snakeLength = 5;
+    snakeSize = 30;
+    snakeDirection = "down";
     
-    for(var index = 0; index < snakeLength; index++ ) {
+    for(var index = snakeLength - 1; index >- 0; index--) {
+       snake.push( {
+           x: index,
+           y: 0
+       });
+    }
  }
 
 function snakeDraw() {
-    
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+   }
 }
 
 function snakeUpdate() {
+      var snakeHeadX = snake[0].x;
+      var snakeHeadY = snake[0].y;
+      
+      if(snakeDirection == "down") {
+          snakeHeadY++;
+      }
+       
+      var snakeTail = snake.pop();
+      snakeTail.x = snakeHeadX;
+      snakeTail.y = snakeHeadY;
+      snake.unshift(snakeTail);
+}
+
+/*############################################################################
+ * Food Functions
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ */
+
+function foodInitialize() {
+    food = {
+        x: 0,
+        y: 0
+    };
+    setFoodPosition();
+}
+
+function foodDraw() {
+    context.fillSyle = "white";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random() * screenWidth);
+    var randomY = Math.floor(Math.random() * screenHeight);
     
+    food.x = randomX;
+    food.y = randomY;
 }
